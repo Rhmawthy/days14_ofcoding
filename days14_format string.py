1375eef315c628b1e4c38cf6239750bcef7628e3
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-24 08:11:45 -0700

    Add files via upload

---
+++ days14_format string.py
@@ -0,0 +1,26 @@
+#format string
+
+#contoh generatif
+nama = "Rahmawati"
+format_str = f"hello {nama}"
+
+print(format_str)
+
+#boolean
+boolean = True
+format_str = f"ini boolean {boolean}"
+
+print(format_str)
+
+#angka
+angka = 2.5
+format_str = f"angka = {angka}"
+print(format_str)
+
+# bilangan bulat ribuan
+angka = 50000
+format_str = f"angka = {angka:,}"
+print(format_str)
+
+
+
